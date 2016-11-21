f83533d9343c37ecf3309fd50e3890cf60d3ce0f
Matthew Bae <mbae@hmc.edu>
2016-11-21 13:01:33 -0800

    Finally added files

--- src/main/scala/piconot/internal/PicoBotFunctionality.scala
+++
@@ -1,5 +0,0 @@
-package piconot.internal
-
-class PicoBotFunctionality {
-  
-}
--- src/main/scala/piconot/internal/Program.scala
+++
@@ -1,6 +0,0 @@
-package piconot.internal
-package piconot.internal.globalFunctions
-
-class Program {
-  "0N~S" -> "go S changeState 1"
-}
